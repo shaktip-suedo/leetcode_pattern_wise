d720c0ff372a5dc491ec2e596edfa289f82d925d
Shakti Verma <shakti.verma890@mpgi.edu.in>
2026-07-25 10:02:37 +0530

    Time: 14 ms (76.47%), Space: 47.7 MB (93.35%) - LeetHub

---
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        int n = s.length();
+        if(n %2 != 0){
+            return false;
+        }
+        int balance = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(locked.charAt(i) == '0' || s.charAt(i)=='('){
+                balance++;
+            }else{
+                balance--;
+            }
+            if(balance < 0) return false;
+        }
+        balance = 0 ;
+        for(int i = n-1 ; i >= 0 ; i--){
+            if(locked.charAt(i) == '0' || s.charAt(i)==')'){
+                balance++;
+            }else{
+                balance--;
+            }
+            if(balance < 0) return false;
+        }
+        return true;
+    }
+}
