0a1665c8ef4c76070bceb02080411b86777997ab
Shakti Verma <shakti.verma890@mpgi.edu.in>
2026-08-08 00:15:48 +0530

    Time: 1 ms (96.19%), Space: 43.9 MB (62.82%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> res = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        for(int i = 0 ; i< candies.length; i++){
+            if(candies[i] > max){
+                max = candies[i];
+            }
+        }
+        for(int i = 0 ; i < candies.length; i++){
+            if(candies[i] + extraCandies >=  max){
+                res.add(true);
+            }else{
+                res.add(false);
+            }
+        }
+        return res;
+    }
+}
